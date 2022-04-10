bf08f5fd0e0ead4024c139cc51aa32b0f6002653
MisakaMikoto128 <liuyuanlins@outlook.com>
2022-04-10 17:05:31 +0800

    ADC和串口

---
+++ keil/RTE/_adc0_singleended_multichannel_pwm0gen1trigger/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'adc0_singleended_multichannel_pwm0gen1trigger' 
+ * Target:  'adc0_singleended_multichannel_pwm0gen1trigger' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "msp.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
